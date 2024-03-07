3947705c9c0216a60897f840b73fb2d783a366b4
Mordecai Mengesteab <52512368+MordecaiM24@users.noreply.github.com>
2024-03-07 15:27:06 +0000

    Added email stuff & the edge function. Really hope this works.

--- src/signUp/SignUp.jsx
+++ src/signUp/SignUp.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>Sign Up</div>;
+  return (
+    <div>
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+
+          await axios
+            .post("/api/send-email", {
+              firstName: "Mordecai",
+              lastName: "Mengesteab",
+            })
+            .then((response) => {
+              // Success! Display confirmation message or similar
+            })
+            .catch((error) => {
+              // Error handling
+              console.log("FUCK");
+            });
+        }}
+      >
+        Click me!
+      </button>
+    </div>
+  );
 };
 
 export default SignUp;
